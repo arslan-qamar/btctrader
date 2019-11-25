31332f7a8e9907da5e0f50321c8cf50f5641d146
Arslan Qamar <ravian720@gmail.com>
2019-11-26 08:23:19 +1100

    First Commit

---
+++ BTCTrader/BTCTrader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTCTrader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
